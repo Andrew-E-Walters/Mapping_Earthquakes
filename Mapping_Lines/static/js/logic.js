d0c58dee428f4ffae0f5e0b5dc3c6cacc007c7a0
Andrew-E-Walters <109561310+Andrew-E-Walters@users.noreply.github.com>
2022-10-21 14:08:02 -0400

    adding GeoJSONMaps

--- Mapping_Lines/static/js/logic.js
+++ Mapping_Lines/static/js/logic.js
@@ -4,16 +4,24 @@ console.log("working");
 // Create the map object with a center and zoom level.
 let map = L.map("mapid", {
     center: [
-      40.7, -94.5
+      37.6213,  -122.3790
     ],
-    zoom: 14
+    zoom: 5
   });
 
-//  Add a marker to the map for Los Angeles, California.
-let marker = L.circleMarker([34.0522, -118.2437], {
-radius: 300,
-color: "black",
-fillColor:'#ffffa1"'
+  // Coordinates for each point to be used in the line.
+// Coordinates for each point to be used in the polyline.
+let line = [
+  [33.9416, -118.4085],
+  [37.6213, -122.3790],
+  [40.7899, -111.9791],
+  [47.4502, -122.3088]
+];
+
+
+// Create a polyline using the line coordinates and make the line yellow.
+L.polyline(line, {
+  color: "yellow"
 }).addTo(map);
 
 
